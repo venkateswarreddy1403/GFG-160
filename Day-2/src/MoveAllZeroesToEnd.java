30a4f47a8ab84ac5857a22e90f0066afa8207741
venkateswara1403 <venkateswarareddy1403@gmail.com>
2024-12-20 10:46:19 +0530

    Solved to upto day-3

---
+++ Day-2/src/MoveAllZeroesToEnd.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class MoveAllZeroesToEnd {
+    static void pushZerosToEnd(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[count];
+                arr[count] = temp;
+                count++;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = in.nextInt();
+        }
+        pushZerosToEnd(arr);
+    }
+}
---
+++ Day-3/src/ReverseAnArray.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class ReverseAnArray {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = in.nextInt();
+        }
+        reverseArray(arr);
+    }
+    static void reverseArray(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
